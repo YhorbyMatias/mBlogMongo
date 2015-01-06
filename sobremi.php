34e09981bde08dadfc1d4c718848802233aa225b
esneyder <edesalla17@gmail.com>
2015-01-06 09:32:23 -0500

    blogg

--- sobremi.php
+++ sobremi.php
@@ -23,7 +23,7 @@
    Estudiante de Ingeniería en Software, amante de la seguridad informática y desarrollador.<br>
    Mis pasión por el desarrollo de Software hace que este constantemente investigando asuntos 
    relacionados que me fortalezcan cada día en las diferentes áreas de esta hermosa profesión.<br>
-    <a href="ingedwinesneyder.wordpress.com" target="_blank">Blog</a> <strong> <=></strong>
+    <a href="https://ingedwinesneyder.wordpress.com" target="_blank">Blog</a> <strong> <=></strong>
    <a href="http://ingedwinesneyder.hol.es" target="_blank">Pagina</a>  <strong> <=></strong>
    <a href="https://www.youtube.com/channel/UCCghAQHliodXURGl8d67UCw/videos">Canal Youtube</a>
    <div id="fb-root"></div>
